1896f67bdf00e57b7adbc586c947d34a2bdea02e
ThSei <ThSei@users.noreply.github.com>
2015-06-08 10:22:06 +0200

    Create codeeval_primepalindrome.py

---
+++ codeeval_primepalindrome.py
@@ -0,0 +1,28 @@
+def is_prime(n):
+	if n == 2 or n == 3:
+		return True
+	if n < 2 or n % 2 == 0:
+		return False
+	if n < 9:
+		return True
+	if n % 3 == 0:
+		return False
+	r = int(n**0.5)
+	f = 5
+	while f <= r:
+		#print '\t', f
+		if n % f == 0:
+			return False
+		if n % (f + 2) == 0:
+			return False
+		f += 6
+	return True
+
+
+for num in range(1000, 3, -1):
+#for num in range(100, 1001, 1):
+#for num in range(1001):
+	if str(num) == str(num)[::-1] and is_prime(num): 	#inverts the string
+	#if str(num) == str(num)[::-1]: 	#inverts the string
+		print num
+		break;
